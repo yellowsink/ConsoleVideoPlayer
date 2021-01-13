d8a08d4e90ad25f974edb722a7f01b93671e5991
cainy-a <cainy-a@users.noreply.github.com>
2021-01-13 22:32:32 +0000

    Convert Color to ConsoleColor, convert a list of colours into ColouredCharacter objects

--- ConsoleVideoPlayer.Img2Text/Converter.cs
+++ ConsoleVideoPlayer.Img2Text/Converter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using ImageMagick;
 
@@ -6,9 +7,11 @@ namespace ConsoleVideoPlayer.Img2Text
 {
 	public class Converter
 	{
+		public static readonly char[] IntensityChars = {' ', '░', '▓', '▒', '█'};
+
 		public string ImagePath { get; init; }
 
-		public BlockSize CalculateBlockSize(int targetWidth, int targetHeight)
+		private BlockSize CalculateBlockSize(int targetWidth, int targetHeight)
 		{
 			var img           = new MagickImage(ImagePath);
 			var currentWidth  = img.BaseWidth;
@@ -29,7 +32,10 @@ namespace ConsoleVideoPlayer.Img2Text
 			};
 		}
 
-		public Color[] GetColoursOfFrame(int? targetWidth = null, int? targetHeight = null)
+		/// <summary>
+		///     Gets the colours of the frame
+		/// </summary>
+		public KeyValuePair<Coordinate, Color>[] GetColoursOfFrame(int? targetWidth = null, int? targetHeight = null)
 		{
 			var img = new MagickImage(ImagePath);
 			targetWidth  ??= img.BaseWidth;
@@ -37,7 +43,7 @@ namespace ConsoleVideoPlayer.Img2Text
 			var blockSize = CalculateBlockSize(targetWidth.Value, targetHeight.Value);
 			var processor = new ImageProcessor {ImagePath = ImagePath};
 
-			var working = new List<Color>();
+			var working = new List<KeyValuePair<Coordinate, Color>>();
 			for (var i = 0; i < targetWidth; i++)
 			for (var j = 0; j < targetHeight; j++)
 			{
@@ -51,18 +57,79 @@ namespace ConsoleVideoPlayer.Img2Text
 				var endX   = startX + blockWidth;
 				var endY   = startY + blockHeight;
 
-				working.Add(processor.AverageColourInArea(startX, startY, endX, endY));
+				working.Add(new KeyValuePair<Coordinate, Color>(new Coordinate(i, j),
+				                                                processor.AverageColourInArea(
+					                                                startX, startY, endX, endY)));
+			}
+
+			return working.ToArray();
+		}
+
+		/// <summary>
+		///     Based on the intensity of each color in the provided set gets a suitable character from IntensityChars for each
+		/// </summary>
+		public KeyValuePair<Coordinate, ColouredCharacter>[] ColoursToCharacters(
+			IEnumerable<KeyValuePair<Coordinate, Color>> colours)
+		{
+			var working = new List<KeyValuePair<Coordinate, ColouredCharacter>>();
+			foreach (var (coordinate, colour) in colours)
+			{
+				var intensity = (colour.R + colour.G + colour.B) / 3;
+
+				// ReSharper disable once PossibleLossOfFraction
+				var scaledIntensity       = (int) Math.Round((double) (intensity / IntensityChars.Length));
+				var offsetScaledIntensity = scaledIntensity - 1;
+				var intensityChar         = IntensityChars[offsetScaledIntensity];
+
+				working.Add(new KeyValuePair<Coordinate, ColouredCharacter>(coordinate, new ColouredCharacter
+				{
+					Character = intensityChar,
+					Colour    = colour.ClosestConsoleColor()
+				}));
 			}
 
 			return working.ToArray();
 		}
 	}
 
-	public class BlockSize
+	public class ColouredCharacter
 	{
+		public char         Character;
+		public ConsoleColor Colour;
+	}
+
+	public class Coordinate
+	{
+		public int X;
+		public int Y;
+
+		public Coordinate(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+
+	internal class BlockSize
+	{
+		/// <summary>
+		///     The height of each block
+		/// </summary>
 		public int BaseHeight;
+
+		/// <summary>
+		///     The width of each block
+		/// </summary>
 		public int BaseWidth;
+
+		/// <summary>
+		///     The height of the last block to account for unclean dividing
+		/// </summary>
 		public int LastHeight;
+
+		/// <summary>
+		///     The width of the last block to account for unclean dividing
+		/// </summary>
 		public int LastWidth;
 	}
 }
