9350743839f8823ce25f470b073ce69bec751199
cainy-a <cainy-a@users.noreply.github.com>
2021-01-13 14:56:09 +0000

    Split video into images

--- ConsoleVideoPlayer.Tests/PreProcessorTests.cs
+++ ConsoleVideoPlayer.Tests/PreProcessorTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using ConsoleVideoPlayer.VideoProcessor;
 using NUnit.Framework;
@@ -10,11 +11,21 @@ namespace ConsoleVideoPlayer.Tests
 	{
 		private string _testVideoPath;
 
-		[SetUp]
+		private string _tempFolder;
+
+		[SetUp] // Setup is called before each test
 		public void Setup()
 		{
 			_testVideoPath = Path.Combine(Environment.CurrentDirectory, "test_vid.mp4");
+			_tempFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+			                           @"Temp\Cain Atkinson\ConsoleVideoPlayer");
+			CleanupTempFolder();
 		}
+
+		private void CleanupTempFolder() => Directory.Delete(_tempFolder, true);
+
+		[TearDown] // TearDown is called after each test
+		public void Teardown() => CleanupTempFolder();
 
 		[Test]
 		public async Task ExtractAudioTest()
@@ -26,8 +37,27 @@ namespace ConsoleVideoPlayer.Tests
 
 			try
 			{
-				var path = await preProcessor.ExtractAudio();
+				var path = await preProcessor.ExtractAudio(true);
 				Assert.True(File.Exists(path));
+			}
+			catch (Exception ex)
+			{
+				Assert.Fail(ex.Message);
+			}
+		}
+
+		[Test]
+		public async Task SplitVideoIntoImagesTest()
+		{
+			var preProcessor = new PreProcessor
+			{
+				VideoPath = _testVideoPath
+			};
+
+			try
+			{
+				var path = await preProcessor.SplitVideoIntoImages();
+				Assert.True(Directory.EnumerateFiles(path).Any());
 			}
 			catch (Exception ex)
 			{
--- ConsoleVideoPlayer.VideoProcessor/PreProcessor.cs
+++ ConsoleVideoPlayer.VideoProcessor/PreProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Xabe.FFmpeg;
 
@@ -19,16 +20,14 @@ namespace ConsoleVideoPlayer.VideoProcessor
 		///     Extracts the audio as WAV from a video into a temp folder
 		/// </summary>
 		/// <returns>The path to the audio file</returns>
-		public async Task<string> ExtractAudio()
+		public async Task<string> ExtractAudio(bool overwrite = false)
 		{
 			var tempFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-			                              "Temp\\Cain Atkinson\\ConsoleVideoPlayer");
-			Directory.CreateDirectory(tempFolder);
-
+			                              @"Temp\Cain Atkinson\ConsoleVideoPlayer");
 			var audioPath    = Path.Combine(tempFolder, "ExtractedAudio");
 			var audioPathWav = $"{audioPath}.wav";
 
-			await ExtractAudio(audioPath);
+			await ExtractAudio(audioPath, overwrite);
 
 			return audioPathWav;
 		}
@@ -36,13 +35,47 @@ namespace ConsoleVideoPlayer.VideoProcessor
 		/// <summary>
 		///     Extracts the audio as WAV from a video into the specified folder
 		/// </summary>
-		public async Task ExtractAudio(string destination)
+		public async Task ExtractAudio(string destination, bool overwrite = false)
 		{
 			var audioPathMkv = $"{destination}.mkv";
 			var audioPathWav = $"{destination}.wav";
+
+			if (overwrite) File.Delete(audioPathWav);
+
 			await (await FFmpeg.Conversions.FromSnippet.ExtractAudio(VideoPath, audioPathMkv)).Start();
 			await (await FFmpeg.Conversions.FromSnippet.Convert(audioPathMkv, audioPathWav)).Start();
 			File.Delete(audioPathMkv);
+		}
+
+		/// <summary>
+		///     Extracts all the images as PNG in a video into a temp folder
+		/// </summary>
+		/// <returns>The path to the folder containing the images</returns>
+		public async Task<string> SplitVideoIntoImages(bool overwrite = false)
+		{
+			var tempFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+			                              @"Temp\Cain Atkinson\ConsoleVideoPlayer\Split Images");
+			await SplitVideoIntoImages(tempFolder, overwrite);
+			return tempFolder;
+		}
+
+		/// <summary>
+		///     Extracts all the images as PNG in a video into the specified folder
+		/// </summary>
+		public async Task SplitVideoIntoImages(string destination, bool overwrite = false)
+		{
+			if (overwrite) Directory.Delete(destination, true);
+			Directory.CreateDirectory(destination);
+
+			string OutputFileNameBuilder(string i) => $"\"{Path.Combine(destination, $"image{i}.png")}\"";
+
+			var info        = await FFmpeg.GetMediaInfo(VideoPath).ConfigureAwait(false);
+			var videoStream = info.VideoStreams.First()?.SetCodec(VideoCodec.png);
+
+			await FFmpeg.Conversions.New()
+			            .AddStream(videoStream)
+			            .ExtractEveryNthFrame(1, OutputFileNameBuilder)
+			            .Start();
 		}
 	}
 }
