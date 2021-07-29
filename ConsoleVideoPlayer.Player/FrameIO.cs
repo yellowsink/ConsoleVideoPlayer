16655fc63aeff127bb3fe826450b077ce47603a7
Cain Atkinson <yellowsink@protonmail.com>
2021-07-29 08:19:47 +0100

    Quick code cleanup

--- ConsoleVideoPlayer.Player/FrameIO.cs
+++ ConsoleVideoPlayer.Player/FrameIO.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.CompilerServices;
 using MessagePack;
 using MessagePack.Resolvers;
 
@@ -9,22 +8,26 @@ namespace ConsoleVideoPlayer.Player
 	// ReSharper disable once InconsistentNaming
 	public static class FrameIO
 	{
-		public static void Save(this ParsedCvidFile frames, string savePath) => MessagePackSerializer.Serialize(File.Create(savePath), frames, ContractlessStandardResolver.Options);
+		public static void Save(this ParsedCvidFile frames, string savePath)
+			=> MessagePackSerializer.Serialize(File.Create(savePath), frames, ContractlessStandardResolver.Options);
 
-		public static ParsedCvidFile ReadFrames(string savePath) => MessagePackSerializer.Deserialize<ParsedCvidFile>(File.OpenRead(savePath));
+		public static ParsedCvidFile ReadFrames(string savePath)
+			=> MessagePackSerializer.Deserialize<ParsedCvidFile>(File.OpenRead(savePath));
 	}
 
 	[MessagePackObject]
 	public class ParsedCvidFile
 	{
+		[Key("audio")] public byte[]        Audio;
+		[Key("rate")]  public double        Framerate;
+		[IgnoreMember] public Queue<string> Frames = new();
+
 		[Key("frames")]
+		// ReSharper disable once UnusedMember.Global
 		public string[] FrameArray
 		{
-			set => Frames = new(value);
+			set => Frames = new Queue<string>(value);
 			get => Frames.ToArray();
 		}
-		[IgnoreMember] public Queue<string> Frames = new();
-		[Key("rate")]  public double        Framerate;
-		[Key("audio")] public byte[]        Audio;
 	}
 }
