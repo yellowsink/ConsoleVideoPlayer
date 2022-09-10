c2df54c4c85c79acb8a1b4d0461e20a1c15709ca
yellowsink <yellowsink@riseup.net>
2022-09-10 17:09:33 +0100

    move to td lib

--- ConsoleVideoPlayer.Player/Player.cs
+++ ConsoleVideoPlayer.Player/Player.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using ConsoleVideoPlayer.MediaProcessor;
 
@@ -16,7 +14,7 @@ public static class Player
 	// how many (at most!) frames must be remaining before the next batch is queued for processing
 	public const int FrameProcessThres = 2;
 
-	public static async Task PlayAsciiFrames(IFrameStream cstream, double frameRate, bool debug, int frameSkip)
+	public static Task PlayAsciiFrames(IFrameStream cstream, double frameRate, bool debug, int frameSkip)
 	{
 		var stats = new RunningStats();
 
@@ -34,80 +32,45 @@ public static class Player
 			Console.Write(stats.Render(timeDebt.Value));
 		}
 
-		await GenericPlay(cstream, Console.Write, frameRate, frameSkip, debug ? DebugFunc : null);
+		return GenericPlay(cstream, Console.Write, frameRate, frameSkip, debug ? DebugFunc : null);
 	}
 
-	public static async Task PlayKittenFrames(IFrameStream filePaths, double frameRate, int frameSkip)
-	{
-		await GenericPlay(filePaths,
-						  path =>
-						  {
-							  var b64Path = Convert.ToBase64String(Encoding.Default.GetBytes(path));
-							  
-							  // a=T = magical param that makes it work (?)
-							  // f=100 = png
-							  // t=f = read from file
-							  Console.Write($"\u001b_Gq=1,a=T,f=100,t=f;{b64Path}\u001b\\");
-						  },
-						  frameRate,
-						  frameSkip);
-	}
+	public static Task PlayKittenFrames(IFrameStream filePaths, double frameRate, int frameSkip)
+		=> GenericPlay(filePaths,
+					   path =>
+					   {
+						   var b64Path = Convert.ToBase64String(Encoding.Default.GetBytes(path));
+
+						   // a=T = magical param that makes it work (?)
+						   // f=100 = png
+						   // t=f = read from file
+						   Console.Write($"\u001b_Gq=1,a=T,f=100,t=f;{b64Path}\u001b\\");
+					   },
+					   frameRate,
+					   frameSkip);
 
 	[MethodImpl(MethodImplOptions.AggressiveOptimization)]
-	private static async Task GenericPlay(IFrameStream cstream,   Action<string> renderFunc, double frameRate,
-										  int          frameSkip, Action<long?>? debugFunc = null)
-	{
-		var frameTime   = (long) (10_000_000 / frameRate);
-		var startTime   = DateTime.UtcNow.Ticks;
-		var timeDebt    = 0L;
-		var skipCounter = 0;
-
-		Console.CursorVisible = false;
-
-		for (var frameCount = 1; cstream.Count > 0; frameCount++)
-		{
-			// keep conversion stream in check
-			if (cstream.ReadyCount < FrameProcessThres)
-				cstream.SafelyProcessMore(FrameBatchSize);
-			
-			if (timeDebt > frameTime)
-			{
-				if (frameSkip == -1 || skipCounter < frameSkip)
-				{
-					skipCounter++;
-					timeDebt -= frameTime;
-					await cstream.GetAsync();
-					debugFunc?.Invoke(null);
-					continue;
-				}
-
-				//else
-				skipCounter = 0;
-			}
-
-			Console.Write("\u001b[H");
-			renderFunc(await cstream.GetAsync());
-			debugFunc?.Invoke(timeDebt);
-
-			// measure current time
-			var current  = DateTime.UtcNow.Ticks - startTime;
-			
-			// the full amount of time we are behind by
-			var amountBehind = current - (frameCount * frameTime) + timeDebt;
-			
-			// timeDebt has been accounted for, reset it!
-			timeDebt = 0;
-			
-			// ideally how long we need to wait for (how far *ahead* we are if you will)
-			var waitTime = frameTime - amountBehind;
-
-			if (waitTime > 0)
-				Thread.Sleep(new TimeSpan(waitTime));
-			else
-				// if we can't fully make up time try to do it later
-				timeDebt += -waitTime;
-		}
-		
-		Console.CursorVisible = true;
-	}
+	private static Task GenericPlay(IFrameStream   cstream, Action<string> renderFunc, double frameRate, int frameSkip,
+									Action<long?>? debugFunc = null)
+		=> TimeDebt.TimeDebt.TrackTimeAsync(frameRate,
+											async timeDebt =>
+											{
+												Console.Write("\u001b[H");
+												renderFunc(await cstream.GetAsync());
+												debugFunc?.Invoke(timeDebt);
+											},
+											_ => cstream.Count > 0,
+											(Func<Task>?) (frameSkip == 0
+															   ? null
+															   : async () =>
+															   {
+																   await cstream.GetAsync();
+																   debugFunc?.Invoke(null);
+															   }),
+											() =>
+											{
+												if (cstream.ReadyCount < FrameProcessThres)
+													cstream.SafelyProcessMore(FrameBatchSize);
+												return Task.CompletedTask;
+											});
 }
